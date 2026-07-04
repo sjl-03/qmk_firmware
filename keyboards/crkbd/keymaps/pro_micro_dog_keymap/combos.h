81ed0791522e7095b4e80f1a71795aacb47fb421
SL <81534940+sjl-03@users.noreply.github.com>
2026-07-05 00:00:30 +0100

    delete files

--- keyboards/crkbd/keymaps/pro_micro_dog_keymap/combos.h
+++
@@ -1,9 +0,0 @@
-// Copyright 2026
-// SPDX-License-Identifier: GPL-2.0-or-later
-//
-// Not in your original file list, but combos deserve their own file for
-// the same reason everything else got split out. Requires COMBO_ENABLE
-// in rules.mk and COMBO_COUNT / COMBO_TERM in config.h.
-#pragma once
-
-#include QMK_KEYBOARD_H
